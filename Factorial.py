e74df8759b0bc491273967508c0759791f33d6e9
arya-15126 <aryaadhokshajvrao@gmail.com>
2026-09-02 12:39:29 +0530

    done

--- Factorial.py
+++ Factorial.py
@@ -1,1 +1,24 @@
 
+
+
+number = 5
+
+
+factorial = 1
+
+
+if number < 0:
+    print("Factorial is not defined for negative numbers.")
+
+else:
+ 
+    for i in range(1, number + 1):
+        factorial = factorial * i
+
+   
+    print("======================================")
+    print("          FACTORIAL CALCULATOR")
+    print("======================================")
+    print("Given Number :", number)
+    print("Factorial    :", factorial)
+    print("======================================")
